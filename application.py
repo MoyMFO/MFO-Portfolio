67a9ae6aafcac4c72c7773a0bb8e6e4e3a7a51b0
Moises Flores Ortiz <floresmoisesortiz@gmail.com>
2024-01-05 18:21:49 -0600

    testing bootstrap and files

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap
 
 application = Flask(__name__)
+bootstrap = Bootstrap(application)
 
 @application.route("/")
 def hello_world():
-    return 'Hello World'
+    return render_template('index.html')
 
 @application.route("/hello")
 def hello_world_2():
